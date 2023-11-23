31de4c7fafcdd4b010a94e02f24414e84e4c6407
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-23 16:39:52 -0400

    Added migration

---
+++ database/migrations/2023_11_23_203211_create_consulting_rooms_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('consulting_rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('zip');
+            $table->string('street');
+            $table->string('colony');
+            $table->string('state');
+            $table->string('delegation');
+            $table->string('n_exterior');
+            $table->string('n_interior')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('logo')->nullable();
+            $table->tinyInteger('desing');
+            
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('consulting_rooms');
+    }
+};
