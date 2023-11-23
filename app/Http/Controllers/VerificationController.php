7d8bb37a7d78a7a722143b2ff92e8e45dab4a499
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-23 16:08:35 -0400

    Added functions

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 class VerificationController extends Controller
 {
-    //
+    public function verify(EmailVerificationRequest  $request): JsonResponse
+    {
+        $request->fulfill();
+        return response()->json();
+    }
+    public function resend(Request $request): JsonResponse
+    {
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        $user = User::where('email', $request->email)->firstOrFail();
+        $user->sendEmailVerificationNotification();
+        return response()->json();
+    }
 }
