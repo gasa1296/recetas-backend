f61ad9e25e58e4abf7a6b76096ffd706f3c8269b
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-23 21:12:51 -0400

    Added migration

---
+++ database/migrations/2023_11_24_010722_create_patients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('fist_name');
+            $table->string('last_name1');
+            $table->string('last_name2')->nullable();
+            $table->string('email');
+            $table->string('phone1');
+            $table->string('phone2')->nullable();
+            $table->date('birth_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
