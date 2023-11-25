7d1d1ffe4f6e8977754a0c5d1f37cfec795cac12
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-25 03:48:18 -0400

    Added Controller

---
+++ app/Http/Controllers/PrescriptionMedicamentController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PrescriptionMedicament;
+use Illuminate\Http\Request;
+
+class PrescriptionMedicamentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PrescriptionMedicament $prescriptionMedicament)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PrescriptionMedicament $prescriptionMedicament)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PrescriptionMedicament $prescriptionMedicament)
+    {
+        //
+    }
+}
