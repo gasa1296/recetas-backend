eda81d74db0997b6576b85192038feb2345cc19a
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-25 03:33:04 -0400

    Added Migration

---
+++ database/migrations/2023_11_25_062830_create_prescription_medicaments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prescription_medicaments', function (Blueprint $table) {
+            $table->string('dose')->nullable();
+            $table->string('way');
+            $table->string('frequency')->nullable();
+            $table->string('duration');
+            $table->foreignId('medicament_id')->constrained();
+            $table->foreignId('prescription_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prescription_medicaments');
+    }
+};
