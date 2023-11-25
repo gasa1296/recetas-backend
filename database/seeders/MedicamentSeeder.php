b83f6a7c6b003d1ac6094d6537ef4f5727679161
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-25 02:24:53 -0400

    Added Seeder

---
+++ database/seeders/MedicamentSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Medicament;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MedicamentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Medicament::factory(10)->create();
+    }
+}
