1a7463895028abe00078b4eb4d122b098aeda009
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-25 00:31:39 -0400

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('fist_name');
+            $table->string('first_name');
             $table->string('last_name1');
             $table->string('last_name2')->nullable();
             $table->string('identification');
