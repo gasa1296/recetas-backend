6296f5d9c84689e8f2d6a83194e5ceb633686848
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-25 02:25:38 -0400

    Updated database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             //UserSeeder::class,
             PatientSeeder::class,
             ConsultingRoomSeeder::class,
+            MedicamentSeeder::class,
         ]);
     }
 }
