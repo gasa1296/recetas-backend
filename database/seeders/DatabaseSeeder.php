7622821cc986a87da4fb78c9a7ea1d1844287a44
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-27 20:28:01 -0400

    Added seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            //UserSeeder::class,
+            UserSeeder::class,
             PatientSeeder::class,
             ConsultingRoomSeeder::class,
             MedicamentSeeder::class,
+            EquipmentSeeder::class,
         ]);
     }
 }
