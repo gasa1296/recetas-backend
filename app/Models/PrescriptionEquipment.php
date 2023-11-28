cff0da0292cf1e01ccfe40ed484b676c91201dd3
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-27 20:24:11 -0400

    Added Model

---
+++ app/Models/PrescriptionEquipment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class PrescriptionEquipment extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'additional',
+        'equipment_id',
+        'prescription_id',
+    ];
+    /**
+     * Get the equipment of the prescription.
+     */
+    public function equipment(): BelongsTo
+    {
+        return $this->belongsTo(Equipment::class);
+    }
+    /**
+     * Get the prescription of the equipment.
+     */
+    public function prescription(): BelongsTo
+    {
+        return $this->belongsTo(Prescription::class);
+    }
+}
