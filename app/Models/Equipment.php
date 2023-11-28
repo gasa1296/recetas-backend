3ed79f8239770950e9ee8874ec6601817bc85b75
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-27 20:22:09 -0400

    Added model

---
+++ app/Models/Equipment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Equipment extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+    /**
+     * Get the prescriptions of the medicaments.
+     */
+    public function prescriptions(): HasMany
+    {
+        return $this->hasMany(PrescriptionEquipment::class, 'equipment_id');
+    }
+}
