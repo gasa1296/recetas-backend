324830d47277ce271db2916f7ce6d6f8e18b8780
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-27 20:32:50 -0400

    Added Controller

---
+++ app/Http/Controllers/EquipmentController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Equipment;
+use Illuminate\Http\Request;
+
+class EquipmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $instances = Equipment::all();
+        return response()->json($instances->paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @todo upload file
+     * @todo Add validations
+     */
+    public function store(Request $request)
+    {
+        $instance = Equipment::create($request->all());
+        return response()->json($instance);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Equipment $equipment)
+    {
+        return response()->json($equipment);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @todo upload file
+     * @todo Add validations
+     */
+    public function update(Request $request, Equipment $equipment)
+    {
+        $equipment->update($request->all());
+        return response()->json($equipment);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Equipment $equipment)
+    {
+        $equipment->delete();
+        return response()->json();
+    }
+}
