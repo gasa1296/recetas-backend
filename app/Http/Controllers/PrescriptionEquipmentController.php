5bb0a5c5e30fdb91937868b4914548239d4ff414
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-27 20:35:47 -0400

    Added Controller

---
+++ app/Http/Controllers/PrescriptionEquipmentController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Equipment;
+use App\Models\PrescriptionEquipment;
+use App\Models\Prescription;
+use Illuminate\Http\Request;
+
+class PrescriptionEquipmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $instances = PrescriptionEquipment::whereRelation("prescription", "user_id", auth()->id());
+        return response()->json($instances->paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @todo Add validations
+     */
+    public function store(Request $request)
+    {
+        $instance = PrescriptionEquipment::create($request->all());
+        return response()->json($instance);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Prescription $prescription, Equipment $equipment)
+    {
+        $instance = PrescriptionEquipment
+            ::whereRelation("prescription", "user_id", auth()->id())
+            ->where('prescription_id', $prescription->id)
+            ->where('equipment_id', $equipment->id)
+            ->firstOrFail();
+        return response()->json($instance);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @todo Add validations
+     */
+    public function update(Request $request, Prescription $prescription, Equipment $equipment)
+    {
+        $instance = PrescriptionEquipment
+            ::whereRelation("prescription", "user_id", auth()->id())
+            ->where('prescription_id', $prescription->id)
+            ->where('equipment_id', $equipment->id)
+            ->firstOrFail();
+        $instance->update($request->all());
+        return response()->json($prescription);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Prescription $prescription, Equipment $equipment)
+    {
+        $instance = PrescriptionEquipment
+            ::whereRelation("prescription", "user_id", auth()->id())
+            ->where('prescription_id', $prescription->id)
+            ->where('equipment_id', $equipment->id)
+            ->firstOrFail();
+        $instance->delete();
+        return response()->json();
+    }
+}
