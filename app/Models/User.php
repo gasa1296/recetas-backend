75fd3b30ac2f19a9d2ecacdb0c5b5e081dcc9fb6
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-28 23:06:06 -0400

    Fix field name

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array<int, string>
      */
     protected $fillable = [
-        'fist_name',
+        'first_name',
         'last_name1',
         'last_name2',
         'identification',
