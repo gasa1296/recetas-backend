4de00650d6a4067355f071c333f9bac9724dc828
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-30 00:31:04 -0400

    Added route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EquipmentController;
 use App\Http\Controllers\PrescriptionEquipmentController;
 use App\Http\Controllers\PrescriptionMedicamentController;
 use App\Http\Controllers\VerificationController;
@@ -44,6 +45,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
         'patient' => PatientController::class,
         'prescription' => Prescription::class,
         'medicament' => MedicamentController::class,
+        'equipment' => EquipmentController::class,
         'prescription.medicament' => PrescriptionMedicamentController::class,
         'prescription.equipment' => PrescriptionEquipmentController::class,
     ]);
