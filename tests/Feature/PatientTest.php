153fbed99376d3a9803883b868eee4b505f49c88
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-30 00:18:30 -0400

    Added test cases

---
+++ tests/Feature/PatientTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Patient;
+use Laravel\Sanctum\Sanctum;
+use Illuminate\Support\Facades\Hash;
+
+class PatientTest extends TestCase
+{
+    use WithFaker;
+
+    private $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+        Sanctum::actingAs($this->user, ['*']);
+
+    }
+    public function test_insert(): void
+    {
+        $response = $this->post('api/patient', [
+            "fist_name" => fake()->firstName(),
+            "last_name1" => fake()->lastName(),
+            "last_name2" => fake()->lastName(),
+            "email" => fake()->email(),
+            "phone1" => fake()->phoneNumber(),
+            "phone2" => fake()->phoneNumber(),
+            "birth_date" => fake()->date(),
+            'genre' => fake()->randomElement(['M', 'F']),
+        ]);
+
+        $response->assertOk();
+    }
+    public function test_update(): void
+    {
+        $patient = Patient::factory()->create();
+        $response = $this->put('api/patient/' . $patient->id, [
+            "fist_name" => fake()->firstName(),
+            "last_name1" => fake()->lastName(),
+            "last_name2" => fake()->lastName(),
+            "email" => fake()->email(),
+            "phone1" => fake()->phoneNumber(),
+            "phone2" => fake()->phoneNumber(),
+            "birth_date" => fake()->date(),
+            'genre' => fake()->randomElement(['M', 'F']),
+        ]);
+
+        $response->assertOk();
+    }
+    public function test_show(): void
+    {
+        $patient = Patient::factory()->create();
+        $response = $this->get('api/patient/' . $patient->id);
+
+        $response->assertOk();
+    }
+    public function test_list(): void
+    {
+        Patient::factory(10)->create();
+        $response = $this->get('api/patient');
+
+        $response->assertOk();
+        $this->assertCount(10, $response->json()['data']);
+    }
+}
