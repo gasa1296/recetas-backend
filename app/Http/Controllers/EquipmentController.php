b96ab98b37d798420752f8489accf8bf3a177365
gasa1296 <gabriel.asa.1296@outlook.es>
2023-11-30 20:58:57 -0400

    Added Type

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Equipment;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Storage;
 
 class EquipmentController extends Controller
