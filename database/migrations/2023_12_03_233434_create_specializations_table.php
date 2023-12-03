27ca104be768c3f5a9ecee8f4aa3d1722c9cca43
gasa1296 <gabriel.asa.1296@outlook.es>
2023-12-03 19:57:02 -0400

    Added field

--- database/migrations/2023_12_03_233434_create_specializations_table.php
+++ database/migrations/2023_12_03_233434_create_specializations_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('identification')->unique();
             $table->string('university');
             $table->string('logo');
+            $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
     }
