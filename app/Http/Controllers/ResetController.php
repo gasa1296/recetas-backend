6b817b33311c480da905619e027f73d68d4a1ab5
gasa1296 <gabriel.asa.1296@outlook.es>
2023-12-04 10:59:12 -0400

    Experimental Resert password

---
+++ app/Http/Controllers/ResetController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class ResetController extends Controller
+{
+    public function request(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+        return $status === Password::RESET_LINK_SENT
+            ? response()->json(['status' => __($status)])
+            : response()->json()->withErrors(['email' => __($status)]);
+    }
+    public function reset (Request $request) {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+    
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ]);
+    
+                $user->save();
+    
+                event(new PasswordReset($user));
+            }
+        );
+        return $status === Password::PASSWORD_RESET
+            ? response()->json(['status' => __($status)])
+            : response()->json()->withErrors(['email' => __($status)]);
+    }
+}
