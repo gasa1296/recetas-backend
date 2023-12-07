35645aa9eb7b7fbed21080780eca23102f984e79
gasa1296 <gabriel.asa.1296@outlook.es>
2023-12-07 11:07:04 -0400

    Added rsource

---
+++ app/Http/Resources/PatientResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PatientResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'first_name' => $this->first_name,
+            'last_name1' => $this->last_name1,
+            'last_name2' => $this->last_name2,
+            'email' => $this->email,
+            'phone1' => $this->phone1,
+            'phone2' => $this->phone2,
+            'birth_date' => $this->birth_date,
+            'gender' => $this->gender,
+            'prescriptions' => PrescriptionResource::collection($this->prescriptions)
+        ];
+    }
+}
