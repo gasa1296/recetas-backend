8bed4b279d14e1761eaa9b3b8aee079f7c5430e2
gasa1296 <gabriel.asa.1296@outlook.es>
2023-12-07 14:12:20 -0400

    Added relations

---
+++ app/Filament/Resources/UserResource/RelationManagers/RoomsRelationManager.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Forms\Components\{TextInput, Toggle, FileUpload};
+
+class RoomsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'rooms';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('zip')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('street')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('colony')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('state')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('delegation')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('n_exterior')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('n_interior')
+                    ->maxLength(255),
+                TextInput::make('address')
+                    ->maxLength(255),
+                TextInput::make('phone')
+                    ->tel()
+                    ->maxLength(255),
+                FileUpload::make('logo'),
+                Toggle::make('design')
+                    ->required(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+}
