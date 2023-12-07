6be0878a4a9b34df369b0093b9ab938db7e49a48
gasa1296 <gabriel.asa.1296@outlook.es>
2023-12-07 14:15:09 -0400

    Added relation

---
+++ app/Filament/Resources/UserResource/RelationManagers/SpecializationsRelationManager.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Forms\Components\{TextInput, FileUpload};
+
+class SpecializationsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'specializations';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('identification')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('university')
+                    ->required()
+                    ->maxLength(255),
+                FileUpload::make('logo')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+}
