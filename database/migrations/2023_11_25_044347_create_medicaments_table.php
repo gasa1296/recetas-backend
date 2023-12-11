d89827de70a369d638948d1c634fc74abf5dfde0
gasa1296 <gabriel.asa.1296@outlook.es>
2023-12-11 12:15:56 -0400

    Delete field

--- database/migrations/2023_11_25_044347_create_medicaments_table.php
+++ database/migrations/2023_11_25_044347_create_medicaments_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('dose');
             $table->string('quantity');
             $table->string('image')->nullable();
-            $table->boolean('digital')->default(true);
             
             $table->timestamps();
         });
