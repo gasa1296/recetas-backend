5d526edbb2b77d38e131af697183747791160616
gasa1296 <gabriel.asa.1296@outlook.es>
2023-12-11 15:01:02 -0400

    Now field is not nullable

--- database/migrations/2023_11_24_040444_create_prescriptions_table.php
+++ database/migrations/2023_11_24_040444_create_prescriptions_table.php
@@ -28,7 +28,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('room_id')->constrained('consulting_rooms');
             $table->foreignId('patient_id')->constrained('patients');
-            $table->string('file')->nullable();
+            $table->string('file');
             $table->integer('status')->default(0);
             
             $table->timestamps();
