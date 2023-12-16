26ea436f2a65eb165353655e9fbd54713652fe34
gasa1296 <gabriel.asa.1296@outlook.es>
2023-12-16 10:25:45 -0400

    Added styles

--- resources/views/mail/email.blade.php
+++ resources/views/mail/email.blade.php
@@ -1,12 +1,29 @@
 
 <x-mail::message>
   ![A cute cat](https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg)
-  <h1>{{$title}}</h1>
-  <p>{{$message}}</p>
-  <p><a href="{{$url}}"><button>{{$button}}</button></a></p>
+  <h1 class="center h1">{{$title}}</h1>
+  <p class="center p">{{$message}}</p>
+  <p class="center"><a href="{{$url}}"><button class="btn">{{$button}}</button></a></p>
 
 </x-mail::message>
 <style>
+  .h1 {
+    font: normal normal normal 28px/33px Roboto;
+  }
+  .center {
+    text-align: center;
+  }
+  .p {
+    font: normal normal normal 14px/18px Roboto;
+  }
+  .btn {
+    background: #181818;
+    border: 1px solid #181818;
+    border-radius: 8px;
+    color: #fff;
+    padding: 16px 32px;
+    font: normal normal normal 16px/16px Roboto;
+  }
   .logo, .footer p {
     display: none;
   }
