cb362cc0a6fe05c435f91b123d59c4de0784ee12
gasa1296 <gabriel.asa.1296@outlook.es>
2023-12-28 22:05:42 -0400

    Update Patient.php
    Added notifiable trait

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
 
 class Patient extends Model
 {
     use HasFactory, SoftDeletes;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
