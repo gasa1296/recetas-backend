0a47fe814a89d3855ce058eaf47f34577d33eeb9
gasa1296 <gabriel.asa.1296@outlook.es>
2024-01-25 20:45:12 -0400

    Added user to seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -29,6 +29,34 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
             'password' => Hash::make('12345678'),
         ]);
+        User::create([
+            'first_name' => 'admin',
+            'name' => 'admin',
+            'last_name1' => 'admin',
+            'last_name2' => 'admin',
+            'phone1' => fake()->phoneNumber(),
+            'phone2' => fake()->phoneNumber(),
+            'gender' => 'M',
+            'fesa' => fake()->randomNumber(),
+            'email' => 'dan@studio-8.co',
+            'is_admin' => true,
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234567890'),
+        ]);
+        User::create([
+            'first_name' => 'admin',
+            'name' => 'admin',
+            'last_name1' => 'admin',
+            'last_name2' => 'admin',
+            'phone1' => fake()->phoneNumber(),
+            'phone2' => fake()->phoneNumber(),
+            'gender' => 'M',
+            'fesa' => fake()->randomNumber(),
+            'email' => 'victor.hernandez@fanafesa.com',
+            'is_admin' => true,
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234567890'),
+        ]);
         User::factory(10)
             ->has(ConsultingRoom::factory()->count(3), 'rooms')
             ->has(Specialization::factory()->count(3), 'rooms')
