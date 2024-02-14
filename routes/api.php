b6bc2fd8de8ae56f7bc1a458459248f02af0a68e
gasa1296 <gabriel.asa.1296@outlook.es>
2024-02-13 20:50:48 -0400

    Added route

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::controller(AuthController::class)->prefix('auth')->group(function () {
+Route::controller(AuthController::class)->prefix('auth')->name('auth.')->group(function () {
     Route::post('register', 'register');
     Route::post('login', 'login');
     Route::delete('logout', 'logout')->middleware(['auth:sanctum', /*'verified'*/]);
@@ -31,7 +31,7 @@ Route::controller(ResetController::class)->prefix('password')->name('password.')
     Route::post('request', 'request')->name('request');
     Route::post('reset', 'reset')->name('reset');
 });
-Route::controller(PrescriptionController::class)->prefix('receta')->group(function () {
+Route::controller(PrescriptionController::class)->prefix('receta')->name('public_prescription.')->group(function () {
     Route::get('', 'getByClient');
     Route::post('file', 'addFile');
     Route::get('{prescription}', 'show');
@@ -68,6 +68,7 @@ Route::middleware(['auth:sanctum', /*'verified'*/])->group(function () {
     Route::controller(PrescriptionController::class)->prefix('prescription')->name('prescription.')->group(function () {
         Route::get('{prescription}/email', 'sendEmailNotification')->name('email');
         Route::get('{prescription}/sign', 'createSigner')->name('sign');
+        Route::get('{prescription}/document', 'createDocument')->name('document');
         Route::get('{prescription}/file', 'getFile')->name('getFile');
     });
 });
