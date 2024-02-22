6b8a62baecdb2b7e3cde00d943346487607b1253
gasa1296 <gabriel.asa.1296@outlook.es>
2024-02-21 20:25:51 -0400

    Fix

--- app/Http/Controllers/PrescriptionController.php
+++ app/Http/Controllers/PrescriptionController.php
@@ -226,7 +226,6 @@ class PrescriptionController extends Controller
         } else {
             $prescription->status = 1;
         }
-        $prescription->save();
         $prescription->push();
 
         return (new PrescriptionResource($prescription))->response();
