ab7f92b345faae37ac5c983a07b50e92107c27c5
gasa1296 <gabriel.asa.1296@outlook.es>
2024-02-23 20:42:31 -0400

    fix field

--- database/migrations/2023_11_23_211030_create_consulting_rooms_table.php
+++ database/migrations/2023_11_23_211030_create_consulting_rooms_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->string('n_interior')->nullable();
             $table->string('address')->nullable();
             $table->string('phone')->nullable();
-            $table->string('logo');
+            $table->string('logo')->nullable();
             $table->string('design');
             $table->foreignId('user_id')->constrained('users');
 
