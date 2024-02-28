df64f2634a98527b72e7d7cd206789e103980c9e
gasa1296 <gabriel.asa.1296@outlook.es>
2024-02-27 21:11:33 -0400

    Moved to another COntroller

--- app/Http/Controllers/PrescriptionController.php
+++ app/Http/Controllers/PrescriptionController.php
@@ -148,133 +148,6 @@ class PrescriptionController extends Controller
         return response()->json();
     }
     /**
-     * Update the specified resource in storage.
-     */
-    public function addClient(Request $request, Prescription $prescription)
-    {
-        $token = $request->bearerToken();
-        if ($token != env('PUBLIC_KEY', '')) {
-            return response()->json(['token' => 'token invalido'], 403);
-        }
-        if (!empty($prescription->client)) {
-            return response()->json(['client' => 'Ya tiene cliente'], 400);
-        }
-        $validator = Validator::make($request->all(), [
-            'client' => ['required'],
-        ]);
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-        $inputs = $validator->safe()->only('client');
-        $prescription->update($inputs);
-        return (new PrescriptionResource($prescription))->response();
-    }
-    /**
-     * Display a listing of the resource by client.
-     */
-    public function getByClient(Request $request)
-    {
-        $token = $request->bearerToken();
-        if ($token != env('PUBLIC_KEY', '')) {
-            return response()->json(['token' => 'token invalido'], 403);
-        }
-        $validator = Validator::make($request->all(), [
-            'client' => ['required'],
-        ]);
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-        $inputs = $validator->safe()->only('client');
-        return PrescriptionResource::collection(Prescription::where('client', $inputs['client'])->paginate(10))->response();
-    }
-    /**
-     * Display a listing of the resource by client.
-     */
-    public function updateStatus(Request $request, Prescription $prescription)
-    {
-        $token = $request->bearerToken();
-        if ($token != env('PUBLIC_KEY', '')) {
-            return response()->json(['token' => 'token invalido'], 403);
-        }
-        $validator = Validator::make($request->all(), [
-            '*.total_exp' => ['required', 'numeric'],
-        ]);
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-        $completeds = [];
-        $errors = [];
-        $inputs = $validator->safe()->all();
-        foreach ($prescription->medicaments as $medicament) {
-            $med_id = $medicament->medicament_id;
-            if (!empty($inputs[$med_id])) {
-                $medicament->quantity_exp += $inputs[$med_id]['total_exp'];
-                if ($medicament->group == 'RESTRICCION ANTIBIOTICOS') {
-                    if ($medicament->quantity_exp > $medicament->quantity) {
-                        $errors[$med_id . '.total_exp'] = 'No se puede expedir mas de lo recetado';
-                        continue;
-                    }
-                    if ($medicament->quantity_exp == $medicament->quantity) {
-                        $completeds[$med_id] = true;
-                    }
-                }
-            }
-        }
-        if (!empty($errors)) {
-            return response()->json($errors, 400);
-        }
-        if (!empty($completeds) && $prescription->medicaments->count() == count($completeds)) {
-            $prescription->status = 2;
-        } else {
-            $prescription->status = 1;
-        }
-        $prescription->save();
-        $prescription->push();
-
-        return (new PrescriptionResource($prescription))->response();
-    }
-    /**
-     * Display a listing of the resource by client.
-     */
-    public function addFile(Request $request)
-    {
-        $inputs = $request->all();
-        Log::debug('document', ['document' => $inputs['document']]);
-        $token = $request->bearerToken();
-        if ($token != env('PUBLIC_KEY', '')) {
-            return response()->json(['token' => 'token invalido'], 403);
-        }
-        $instance = Prescription::where('document_id', $inputs['document']['id'])
-            ->firstOrFail();
-        $inputs = $request->all();
-        Log::debug('prescription', ['prescription' => $instance->id]);
-        $dir = "medics/$instance->user_id/prescriptions/$instance->id.zip";
-        if (!Storage::put($dir, base64_decode($inputs['zip']))) {
-            return response()->json('Error guardando archivo', 500);
-        }
-        $instance->file = env('APP_URL') . '/api/receta/' . $instance->id . '/file';
-        $instance->save();
-        Log::debug('prescription', ['file' => $instance->file]);
-        return (new PrescriptionResource($instance))->response();
-    }
-    /**
-     * Display a medicament of the prescription.
-     */
-    public function getMedicament(int $desc)
-    {
-        $reponse = $this->client->post(
-            'https://w9gkg4xp3k.execute-api.us-east-1.amazonaws.com/Prod/api/preproductos',
-            [
-                'json' => [
-                    "hash" => "initial",
-                    "descripcion" => (string) $desc,
-                ]
-            ]
-        );
-        $body = json_decode($reponse->getBody(), true);
-        return response()->json($body['Respuesta'][0]);
-    }
-    /**
      * Send email notification to patient.
      */
     public function sendEmailNotification(Prescription $prescription)
@@ -435,7 +308,7 @@ class PrescriptionController extends Controller
                             [
                                 'key' => 3,
                                 'name' => 'id',
-                                'value' => $prescription->id,
+                                'value' => dechex((new Carbon($prescription->created_at))->timestamp),
                             ]
                         ],
                         [
