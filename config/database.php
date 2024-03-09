e1aea9d7e9dfb883e40a1c8536aaccc90612fd88
gasa1296 <gabriel.asa.1296@outlook.es>
2024-03-08 22:51:14 -0400

    fix

--- config/database.php
+++ config/database.php
@@ -82,7 +82,7 @@ return [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
+            //'port' => env('DB_PORT', '1433'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
@@ -90,7 +90,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            //'trust_server_certificate' => 'false',
         ],
 
     ],
