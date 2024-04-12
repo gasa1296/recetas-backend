d741c8ece3280f5d171c50ff5e1bb73f6fec5827
gasa1296 <gabriel.asa.1296@outlook.es>
2024-04-11 23:28:56 -0400

    Fix color

--- resources/views/mail/prescription.blade.php
+++ resources/views/mail/prescription.blade.php
@@ -1,27 +1,27 @@
 <div style="background-color:#efefef;padding: 20px;text-align: center">
-  <div style="background-color:#ffffff;margin:20px auto;width: 100%; max-width:450px; overflow:hidden">
+  <div style="background-color:white;margin:20px auto;width: 100%; max-width:450px; overflow:hidden">
     <div style="text-align: right;">
       <img src="{{ asset('email/Logo.png') }}" alt="" style="max-width: 300px;">
     </div>
     <div style="background-color: #213d8d; display: flex; justify-content: end;">
-      <p style="font-size:19px; padding:0px 10px;width: 50%;color: #fff; font-weight: bold; text-align: center" >  REVISA TU RECETA MEDICA ELECTRONICA AQUI!</p>
+      <p style="font-size:19px; padding:0px 10px;width: 50%;color: white; font-weight: bold; text-align: center" >  REVISA TU RECETA MEDICA ELECTRONICA AQUI!</p>
     </div>
-    <div style=" display: flex; justify-content: start; margin-top: -180px;">
+    <div style="display: flex; justify-content: start; margin-top: -180px;">
       <img src="{{ asset('email/Fotografia.png') }}" alt="" style="position: relative;max-width: 400px; left: -80px;">
     </div>
     <div style="background-color: #213d8d;"> 
       <div style="position: relative; top: -30px"> 
         <div style="background-color: #f3670f; width: 70%; margin: 0 auto; padding: 5px 10px;"> 
-          <p style="color: fff; font-size: 19px;font-weight:100;text-align: center;">
+          <p style="color: white; font-size: 19px;font-weight:100;text-align: center;">
             Surte tu Receta Electrónica en Farmacias Especializadas ¡la única farmacia que recibe recetas electrónicas y cuida el medio ambiente!
           </p>
-          <p style="color: fff; font-size: 19px;font-weight:500;text-align: center;">
+          <p style="color: white; font-size: 19px;font-weight:500;text-align: center;">
             Para surtir tu receta, descarga nuestra App, regístrate y compra tus medicamentos.
           </p>
           <p style="background-color: #213d8d;width:80%;margin: 0 auto; padding: 10px 5px; color: fff; font-size: 19px;font-weight:500;text-align: center;">
             Si no puedes visualizar tu receta ¡descárgala aqui!
           </p>
-          <p style="color: fff; font-size: 17px;font-weight:lighter;text-align: center;margin-bottom: 0px;">
+          <p style="color: white; font-size: 17px;font-weight:lighter;text-align: center;margin-bottom: 0px;">
             Descarga la App y surte tu receta hoy.
           </p>
           <div>                
