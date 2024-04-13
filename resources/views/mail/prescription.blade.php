c93c4e629ba207ce114faaebd473ea57e1700927
gasa1296 <gabriel.asa.1296@outlook.es>
2024-04-13 12:13:44 -0400

    fix

--- resources/views/mail/prescription.blade.php
+++ resources/views/mail/prescription.blade.php
@@ -1,18 +1,18 @@
 <div style="background-color:#efefef;padding: 20px;text-align: center">
-  <div style="background-color:white;margin:20px auto;width: 100%; max-width:450px; overflow:hidden">
+  <div style="background-color:#ffffff;margin:20px auto;width: 100%; max-width:450px; overflow:hidden">
     <div style="text-align: right;">
       <img src="{{ asset('email/Logo.png') }}" alt="" style="max-width: 300px;">
     </div>
-    <div style="background-color: #213d8d;">
-      <p style="font-size:19px; padding:0px 10px;width: 50%;color: white; font-weight: bold; text-align: center; margin-left:auto" >  REVISA TU RECETA MEDICA ELECTRONICA AQUI!</p>
+    <div style="background-color: #213d8d; display: flex; justify-content: center;">
+      <p style="font-size:19px; margin:0; padding:10px;width: 50%;color: #fff; font-weight: bold;" >  REVISA TU RECETA MEDICA ELECTRONICA AQUI!</p>
     </div>
-    <div style="margin-top: -180px;">
-      <img src="{{ asset('email/Fotografia.png') }}" alt="" style="position: relative;max-width: 400px; left: -80px;">
+    <div style=" display: flex; justify-content: center; ">
+      <img src="test/Fotografia.png" alt="" style="position: relative;max-width: 400px; ">
     </div>
     <div style="background-color: #213d8d;"> 
-      <div> 
-        <div style="background-color: #f3670f; width: 70%; margin: 0 auto; padding: 5px 10px; margin-top: -30px"> 
-          <p style="color: white; font-size: 19px;font-weight:100; text-align: center;">
+      <div style="position: relative; padding-top: 30px;"> 
+        <div style="background-color: #f3670f; width: 70%; margin: 0 auto; padding: 5px 10px;"> 
+          <p style="color: white; font-size: 19px;font-weight:100;text-align: center;">
             Surte tu Receta Electrónica en Farmacias Especializadas ¡la única farmacia que recibe recetas electrónicas y cuida el medio ambiente!
           </p>
           <p style="color: white; font-size: 19px; font-weight:500; text-align: center;">
@@ -21,22 +21,21 @@
           <p style="background-color: #213d8d;width:80%;margin: 0 auto; padding: 10px 5px; color: white; font-size: 19px;font-weight:500;text-align: center;">
             Si no puedes visualizar tu receta ¡descárgala aqui!
           </p>
-          <p style="color: white; font-size: 17px;font-weight:lighter;text-align: center;margin-bottom: 0px;">
+          <p style="color: white; font-size: 17px; font-weight:lighter; text-align: center;margin: 0px; padding-top: 20px;">
             Descarga la App y surte tu receta hoy.
           </p>
           <div>                
             <a target="_blank" href="https://www.youtube.com/">   <img src="{{ asset('email/IOS.png') }}" alt="ios" style="width: 40%"> </a>
             <a target="_blank" href="https://www.youtube.com/"> <img src="{{ asset('email/Googleplay.png') }}" alt="google" style="width: 40%"></a> 
           </div>
-          </div>
-          <div> 
-          <p style="background-color: #213d8d;width:80%;margin: 0 auto; padding: 10px 5px; color: white; font-size: 18px;font-weight:200;text-align: center;">
-            Síguenos en nuestras redes sociales: 
-          </p>
-          <div>                
-            <a target="_blank" href="https://www.youtube.com/">   <img src="{{ asset('email/FACEBOOk.png') }}" alt="ios" style="width: 20%"> </a>
-            <a target="_blank" href="https://www.youtube.com/"> <img src="{{ asset('email/Instagram.png') }}" alt="google" style="width: 20%"></a> 
-          </div>
+        </div>
+        <div> 
+        <p style="background-color: #213d8d;width:80%;margin: 0 auto; padding: 10px 5px; color: white; font-size: 18px;font-weight:200;text-align: center;">
+          Síguenos en nuestras redes sociales: 
+        </p>
+        <div>                
+          <a target="_blank" href="https://www.youtube.com/">   <img src="{{ asset('email/FACEBOOk.png') }}" alt="ios" style="width: 20%"> </a>
+          <a target="_blank" href="https://www.youtube.com/"> <img src="{{ asset('email/Instagram.png') }}" alt="google" style="width: 20%"></a> 
         </div>
       </div>
     </div>
