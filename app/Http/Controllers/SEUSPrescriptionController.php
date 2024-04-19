68e9441c5a5cb02d7fc8d614d70e75c0d682aa9f
gasa1296 <gabriel.asa.1296@outlook.es>
2024-04-19 14:53:19 -0400

    gg

--- app/Http/Controllers/SEUSPrescriptionController.php
+++ app/Http/Controllers/SEUSPrescriptionController.php
@@ -85,15 +85,22 @@ class SEUSPrescriptionController extends Controller
     public function getFile(Request $request, Prescription $prescription)
     {
         $errors = (new PrescriptionController())->verifyPrescription($prescription->medicaments);
+
+        $documents = explode(';', $prescription->document_id);
+        if (count($documents) == 1) {
+            $document = $documents[0];
+        } else {
+            $document = $request->document_id;
+        }
         if (!empty ($errors) || !empty(json_decode($prescription->add_med, true))) {
-            $dir = "medics/$prescription->user_id/prescriptions/$prescription->id-$request->document_id.pdf";
+            $dir = "medics/$prescription->user_id/prescriptions/$prescription->id-$document.pdf";
             return Storage::response($dir, 'receta.pdf', [
                 'Content-Type' => 'application/pdf',
                 'Content-Disposition' => 'inline; filename="receta.pdf"'
             ]);
         } else {
             $zip = new ZipArchive;
-            $dir = "/storage/app/medics/$prescription->user_id/prescriptions/$prescription->id-$request->document_id.zip";
+            $dir = "/storage/app/medics/$prescription->user_id/prescriptions/$prescription->id-$document.zip";
             $status = $zip->open(base_path() . $dir);
             if ($status !== true) {
                 return response()->json('error al obtener archivo 1', 500);
