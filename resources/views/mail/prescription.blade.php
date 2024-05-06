e3944f9e5e0dcdb92215b3bd0a78a3ae598f61f1
gasa1296 <gabriel.asa.1296@outlook.es>
2024-05-06 09:20:50 -0400

    fix

--- resources/views/mail/prescription.blade.php
+++ resources/views/mail/prescription.blade.php
@@ -3,7 +3,7 @@
     <a href="https://www.farmaciasespecializadas.com/?utm_source=email&utm_medium=logo&utm_id=descarga_tu_receta"><img src="{{ asset('email/Logo.png') }}" alt="" style="max-width: 300px;"></a>
   </div>
   <div style="background-color: #213d8d;">
-    <p style="font-size:19px; width: 50%;color: white; font-weight: bold; margin-left:auto; margin-right:auto; text-align: center;" >REVISA TU RECETA MEDICA ELECTRONICA AQUI!</p>
+    <p style="font-size:19px; width: 50%;color: white; font-weight: bold; margin-left:auto; margin-right:auto; text-align: center;text-transform: uppercase;" >REVISA TU RECETA médica electrónica aquí!</p>
   </div>
   <div style="">
     <img src="{{ asset('email/Fotografia.png') }}" alt="" style="max-width: 400px; ">
@@ -15,7 +15,7 @@
           Surte tu Receta Electrónica en Farmacias Especializadas ¡la única farmacia que recibe recetas electrónicas y cuida el medio ambiente!
         </p>
         <p style="color: white; font-size: 19px; font-weight:500; text-align: center;">
-          Para surtir tu receta, descarga nuestra App,  regístrate e ingresa el folio de receta {{$prescription->code}} y compra tus medicamentos.
+          Para surtir tu receta, descarga nuestra App,  regístrate e ingresa el folio de receta <span style="font-size: 21px;">{{$prescription->code}}<span> y compra tus medicamentos.
         </p>
         <p style="background-color: #213d8d;width:80%;margin: 0 auto; padding: 10px 5px; color: white; font-size: 19px;font-weight:500;text-align: center;">
           Si no puedes visualizar tu receta ¡descárgala aqui!
@@ -32,9 +32,9 @@
       <p style="background-color: #213d8d;width:80%;margin: 0 auto; padding: 10px 5px; color: white; font-size: 18px;font-weight:200;text-align: center;">
         Síguenos en nuestras redes sociales: 
       </p>
-      <div style="width:50%">                
-        <a target="_blank" href="https://bit.ly/3Q1QZGw"><img src="{{ asset('email/FACEBOOk.png') }}" alt="ios" style="width: 50%"> </a>
-        <a target="_blank" href="https://bit.ly/49H6ue6"> <img src="{{ asset('email/Instagram.png') }}" alt="google" style="width: 50%"></a> 
+      <div style="width:50%;margin: 0 auto">                
+        <a target="_blank" href="https://bit.ly/3Q1QZGw"><img src="{{ asset('email/FACEBOOk.png') }}" alt="ios" style="width: 45%"> </a>
+        <a target="_blank" href="https://bit.ly/49H6ue6"> <img src="{{ asset('email/Instagram.png') }}" alt="google" style="width: 45%"></a> 
       </div>
     </div>
   </div>
