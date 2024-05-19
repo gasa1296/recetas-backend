3ce3af138abeaeec0afdb2cd9a5e28b27980de66
gasa1296 <gabriel.asa.1296@outlook.es>
2024-05-19 04:55:38 -0400

    fix

--- app/Http/Controllers/PrescriptionMedicamentController.php
+++ app/Http/Controllers/PrescriptionMedicamentController.php
@@ -110,9 +110,9 @@ class PrescriptionMedicamentController extends Controller
     }
     public function mostUsed(): JsonResponse
     {
-        $instances = PrescriptionMedicament::select('salt')->distinct()
+        $instances = PrescriptionMedicament::select('salt')->groupBy('salt')
             ->whereIn('prescription_id', Prescription::select('id')->where('user_id', auth()->id()))
             ->orderByRaw('COUNT(salt) DESC')->limit(10);
-        return response()->json($instances);
+        return response()->json($instances->get());
     }
 }
