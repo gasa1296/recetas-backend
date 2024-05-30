dc81d10e3edc498856caa9ac9f089ddaee66f706
gasa1296 <gabriel.asa.1296@outlook.es>
2024-05-30 15:58:29 -0400

    Fix

--- app/Http/Controllers/MagentoController.php
+++ app/Http/Controllers/MagentoController.php
@@ -74,7 +74,7 @@ class MagentoController extends Controller
 
         ];
         if ($clienteEcommerce = 1) {
-            $req['clienteEcommerce'] = 1;
+            $req['clienteEcommerce'] = 'Sí';
         }
         if (strtoupper($inputs['gender']) == 'M') {
             $req['gender'] = 'Masculino';
