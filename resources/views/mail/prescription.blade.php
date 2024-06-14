34aff280bb2b9785c82973c604d737ca75e9fb4e
gasa1296 <gabriel.asa.1296@outlook.es>
2024-06-14 07:17:57 -0400

    fix

--- resources/views/mail/prescription.blade.php
+++ resources/views/mail/prescription.blade.php
@@ -3,46 +3,60 @@
     <a href="https://www.farmaciasespecializadas.com/?utm_source=email&utm_medium=logo&utm_id=descarga_tu_receta"><img src="{{ asset('email/Logo.png') }}" alt="" style="max-width: 300px;"></a>
   </div>-->
   <div style="background-color: #213d8d;">
-    <p style="font-size:19px; width: 50%;color: white; font-weight: bold; margin-left:auto; margin-right:auto; text-align: center;text-transform: uppercase;" >REVISA TU RECETA médica electrónica aquí!</p>
+    <p
+      style="font-size:19px; width: 50%;color: white; font-weight: bold; margin-left:auto; margin-right:auto; text-align: center;text-transform: uppercase;">
+      REVISA TU RECETA médica electrónica aquí!</p>
   </div>
   <div style="">
     <img src="{{ asset('email/Fotografia.png') }}" alt="" style="max-width: 400px; ">
   </div>
-  <div style="background-color: #213d8d;"> 
-    <div style="padding-top: 30px;"> 
-      <div style="background-color: #f3670f; width: 70%; margin: 0 auto; padding: 5px 10px;"> 
+  <div style="background-color: #213d8d;">
+    <div style="padding-top: 30px;">
+      <div style="background-color: #f3670f; width: 70%; margin: 0 auto; padding: 5px 10px;">
         <p style="color: white; font-size: 19px;font-weight:100;text-align: center;">
-          Surte tu Receta Electrónica en Farmacias Especializadas ¡la única farmacia que recibe recetas electrónicas y cuida el medio ambiente!
+          Surte tu Receta Electrónica en Farmacias Especializadas ¡la única farmacia que recibe recetas electrónicas y
+          cuida el medio ambiente!
         </p>
         <p style="color: white; font-size: 19px; font-weight:500; text-align: center;">
-          Para surtir tu receta, descarga nuestra App,  regístrate e ingresa el folio de receta <span style="font-size: 21px;">{{$prescription->code}}<span> y compra tus medicamentos.
+          Para surtir tu receta, descarga nuestra App, regístrate e ingresa el folio de receta <span
+            style="font-size: 21px;">{{$prescription->code}}<span> y compra tus medicamentos.
         </p>
-        <p style="background-color: #213d8d;width:80%;margin: 0 auto; padding: 10px 5px; color: white; font-size: 19px;font-weight:500;text-align: center;">
-          Si no puedes visualizar tu receta ¡descárgala aqui!
+        <p style="text-align: center;background-color: #213d8d;">
+          <a style="width:80%;margin: 0 auto; padding: 10px 5px; color: white; font-size: 19px;font-weight:500;text-align: center;"
+            href="{{ $link }}">
+            Si no puedes visualizar tu receta ¡descárgala aqui!
+          </a>
         </p>
-        <a href="{{$link}}">
-          <p style="color: white; font-size: 17px; font-weight:lighter; text-align: center;margin: 0px; padding-top: 20px;">
+        <p
+          style="color: white; font-size: 17px; font-weight:lighter; text-align: center;margin: 0px; padding-top: 20px;">
           Descarga la App y surte tu receta hoy.
-          </p>
-        </a>
-        <div style="width:50%;margin: 0 auto">                
-          <a target="_blank" href="https://apple.co/3Q45hq6">   <img src="{{ asset('email/IOS.png') }}" alt="ios" style="width: 45%"> </a>
-          <a target="_blank" href="https://bit.ly/4cRX6Hc"> <img src="{{ asset('email/Googleplay.png') }}" alt="google" style="width: 45%"></a> 
+        </p>
+        <div style="width:50%;margin: 0 auto">
+          <a target="_blank" href="https://apple.co/3Q45hq6"> <img src="{{ asset('email/IOS.png') }}" alt="ios"
+              style="width: 45%"> </a>
+          <a target="_blank" href="https://bit.ly/4cRX6Hc"> <img src="{{ asset('email/Googleplay.png') }}" alt="google"
+              style="width: 45%"></a>
         </div>
       </div>
-      <div> 
-      <p style="background-color: #213d8d;width:80%;margin: 0 auto; padding: 10px 5px; color: white; font-size: 18px;font-weight:200;text-align: center;">
-        Síguenos en nuestras redes sociales: 
-      </p>
-      <div style="width:50%;margin: 0 auto">                
-        <a target="_blank" href="https://bit.ly/3Q1QZGw"><img src="{{ asset('email/FACEBOOk.png') }}" alt="ios" style="width: 45%"> </a>
-        <a target="_blank" href="https://bit.ly/49H6ue6"> <img src="{{ asset('email/Instagram.png') }}" alt="google" style="width: 45%"></a> 
+      <div>
+        <p
+          style="background-color: #213d8d;width:80%;margin: 0 auto; padding: 10px 5px; color: white; font-size: 18px;font-weight:200;text-align: center;">
+          Síguenos en nuestras redes sociales:
+        </p>
+        <div style="width:50%;margin: 0 auto">
+          <a target="_blank" href="https://bit.ly/3Q1QZGw"><img src="{{ asset('email/FACEBOOk.png') }}" alt="ios"
+              style="width: 45%"> </a>
+          <a target="_blank" href="https://bit.ly/49H6ue6"> <img src="{{ asset('email/Instagram.png') }}" alt="google"
+              style="width: 45%"></a>
+        </div>
       </div>
     </div>
-  </div>
 </x-mail::message>
 <style>
-  .logo, .x_header a, .x_footer p, .footer p {
+  .logo,
+  .x_header a,
+  .x_footer p,
+  .footer p {
     display: none;
   }
 </style>
