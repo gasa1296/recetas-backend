517c266118e0210db2d048b9c44335b202da98b2
gasa1296 <gabriel.asa.1296@outlook.es>
2024-07-02 13:16:14 -0400

    fix

--- routes/api.php
+++ routes/api.php
@@ -73,12 +73,7 @@ Route::middleware(['auth:sanctum', /*'verified'*/])->group(function () {
 });
 
 Route::middleware(['auth:sanctum', /*'verified'*/])->name('authenticated.')->group(function () {
-
-    Route::controller(VerificationController::class)->prefix('verification')->name('verification.')->group(function () {
-        Route::get('verify/{id}', 'verify')->name('verify');
-        Route::post('resend', 'resend')->name('resend');
-        Route::get('notice', 'notice')->name('notice');
-    });
+    
     Route::controller(AuthController::class)->prefix('profile')->group(function () {
         Route::get('', 'show')->name('show');
         Route::put('', 'update')->name('update');
