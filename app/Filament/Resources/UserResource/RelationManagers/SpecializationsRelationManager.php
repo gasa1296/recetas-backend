3f4211fcdc1848729b251f25476f5ff81d6f26fe
gasa1296 <gabriel.asa.1296@outlook.es>
2024-07-15 19:47:56 -0400

    Updated

--- app/Filament/Resources/UserResource/RelationManagers/SpecializationsRelationManager.php
+++ app/Filament/Resources/UserResource/RelationManagers/SpecializationsRelationManager.php
@@ -7,7 +7,8 @@ use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Filament\Forms\Components\{TextInput, FileUpload};
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class SpecializationsRelationManager extends RelationManager
 {
@@ -17,18 +18,21 @@ class SpecializationsRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                TextInput::make('name')
+                Forms\Components\TextInput::make('name')
                     ->required()
                     ->maxLength(255),
-                TextInput::make('identification')
-                    ->required()
-                    ->unique()
-                    ->maxLength(255),
-                TextInput::make('university')
+                Forms\Components\TextInput::make('identification')
                     ->required()
                     ->maxLength(255),
-                FileUpload::make('logo')
-                    ->required(),
+                Forms\Components\TextInput::make('university')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\FileUpload::make('logo')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('id_ext')
+                    ->required()
+                    ->maxLength(255)
+                    ->default(0),
             ]);
     }
 
@@ -37,7 +41,32 @@ class SpecializationsRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('name')
             ->columns([
-                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('identification')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('university')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('logo')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('id_ext')
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
