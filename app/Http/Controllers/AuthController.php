58101fd61a7d2711cc6e7bfb4cf7b7e2a4d6699a
gasa1296 <gabriel.asa.1296@outlook.es>
2024-10-10 12:41:58 -0400

    fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,30 +16,33 @@ class AuthController extends Controller
 {
     public function login(Request $request): JsonResponse
     {
-        $instance = User::where(column: 'email', operator: request()->email)->first();
+        $instance = User::where('email', $request->email)->first();
         if (empty($instance)) {
-            $magentoToken = (new MagentoController)->getToken(request: $request);
+            $magentoToken = (new MagentoController)->getToken($request);
             if ($magentoToken->getStatusCode() < 300) {
-                return response()->json(data: [
+                return response()->json([
                     'recetasUser' => false,
                     'magentoEmail' => $request->email
                 ]);
             }
-            return response()->json(data: [['email' => __(key: 'email incorrecto')]], status: 404);
+            return response()->json([['email' => __('email incorrecto')]], 404);
         }
         $okResponse = [
-            'token' => $instance->createToken(name: 'recipe')->plainTextToken,
+            'token' => $instance->createToken('recipe')->plainTextToken,
             'user' => $instance,
         ];
-        if (Hash::check(value: request()->password, hashedValue: $instance->password)) {
-            return response()->json(data: $okResponse);
+        $magento = new MagentoController();
+        if (Hash::check($request->password, $instance->password)) {
+            $medic = $magento->getMedicReq($request->except('password'))->getData(true);
+            return response()->json($okResponse);
         }
-        $magentoToken = (new MagentoController)->getToken(request: $request);
+        $magentoToken = (new MagentoController)->getToken($request);
         if ($magentoToken->getStatusCode() < 300) {
-            return response()->json(data: $okResponse);
+            return response()->json($okResponse);
         }
-        return response()->json(data: [['password' => __(key: 'contraseña incorrecta')]], status: 404);
+        return response()->json([['password' => __('contraseña incorrecta')]], 404);
     }
+
     /**
      * Store a newly created resource in storage.
      */
@@ -86,54 +89,54 @@ class AuthController extends Controller
             'clienteEcommerce' => ['nullable'],
         ]);
         if ($validator->fails()) {
-            return response()->json(data: $validator->errors(), status: 400);
+            return response()->json($validator->errors(), 400);
         }
         $inputs = $validator->safe()->all();
         $magento = new MagentoController();
-        if (!$magento->verifyFESA(fesa: $inputs['fesa'])) {
-            return response()->json(data: ['fesa' => 'Codigo de FESA invalido'], status: 400);
+        if (!$magento->verifyFESA($inputs['fesa'])) {
+            return response()->json(['fesa' => 'Codigo de FESA invalido'], 400);
         }
         if (!empty($inputs['idCX']) && empty($inputs['clienteEcommerce'])) {
-            $res = $magento->store(inputs: $inputs);
-            Log::debug(message: 'magento register', context: $res->getData(assoc: true));
+            $res = $magento->store($inputs);
+            Log::debug('magento register', $res->getData(true));
             if ($res->getStatusCode() >= 300) {
                 return $res;
             }
         } elseif (empty($inputs['idCX']) && empty($inputs['clienteEcommerce'])) {
-            $res = $magento->CX(inputs: $inputs);
-            Log::debug(message: 'cx register', context: $res->getData(assoc: true));
+            $res = $magento->CX($inputs);
+            Log::debug('cx register', $res->getData(true));
             if ($res->getStatusCode() >= 300) {
                 return $res;
             }
-            $inputs['idCX'] = $res->getData(assoc: true)['idCX'];
-            $res = $magento->store(inputs: $inputs);
-            Log::debug(message: 'magento register', context: $res->getData(assoc: true));
+            $inputs['idCX'] = $res->getData(true)['idCX'];
+            $res = $magento->store($inputs);
+            Log::debug('magento register', $res->getData(true));
             if ($res->getStatusCode() >= 300) {
                 return $res;
             }
         }
         if (empty($inputs['password'])) {
-            $inputs['password'] = Hash::make(value: uuid_create(UUID_TYPE_RANDOM));
+            $inputs['password'] = Hash::make(uuid_create(UUID_TYPE_RANDOM));
         }
         $inputs['fesa'] = !empty($inputs['idCX']) ? $inputs['idCX'] : $inputs['fesa'];
-        $instance = User::create(attributes: $inputs);
+        $instance = User::create($inputs);
 
-        event(args: new Registered(user: $instance));
+        event(new Registered($instance));
         foreach ($inputs['rooms'] as $key => $el) {
-            if (!empty($request->file(key: 'logo_room')[$key])) {
-                $file = $request->file(key: 'logo_room')[$key]->store(path: 'medics/' . $instance->id, options: 'public');
+            if (!empty($request->file('logo_room')[$key])) {
+                $file = $request->file('logo_room')[$key]->store('medics/' . $instance->id, 'public');
                 $el['logo'] = $file;
             }
             $el['user_id'] = $instance->id;
-            ConsultingRoom::create(attributes: $el);
+            ConsultingRoom::create($el);
         }
         foreach ($inputs['specializations'] as $key => $el) {
-            if (!empty($request->file(key: 'logo_spec')[$key])) {
-                $file = $request->file(key: 'logo_spec')[$key]->store(path: 'medics/' . $instance->id, options: 'public');
+            if (!empty($request->file('logo_spec')[$key])) {
+                $file = $request->file('logo_spec')[$key]->store('medics/' . $instance->id, 'public');
                 $el['logo'] = $file;
             }
             $el['user_id'] = $instance->id;
-            Specialization::create(attributes: $el);
+            Specialization::create($el);
         }
 
         return response()->json();
@@ -141,16 +144,16 @@ class AuthController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Request $request): JsonResponse
+    public function show(Request $request)
     {
-        return response()->json(data: $request->user());
+        return response()->json($request->user());
     }
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request): JsonResponse
     {
-        $validator = Validator::make(data: $request->all(), rules: [
+        $validator = Validator::make($request->all(), [
             'first_name' => ['required', 'string'],
             'last_name1' => ['required', 'string'],
             'last_name2' => ['nullable', 'string'],
@@ -162,24 +165,24 @@ class AuthController extends Controller
             'fesa' => ['required',],
         ]);
         if ($validator->fails()) {
-            return response()->json(data: $validator->errors(), status: 400);
+            return response()->json($validator->errors(), 400);
         }
         $instance = auth()->user();
         $inputs = $validator->safe()->all();
         if (!empty($inputs['password'])) {
-            $inputs['password'] = Hash::make(value: $inputs['password']);
+            $inputs['password'] = Hash::make($inputs['password']);
         }
-        $instance->update(attributes: $inputs);
+        $instance->update($inputs);
         $inputs = $instance->toArray();
         $inputs['idCX'] = $instance->fesa;
         $inputs['specializations'] = $instance->specializations->toArray();
         $inputs['rooms'] = $instance->rooms->toArray();
 
         $magento = new MagentoController();
-        $magento->CX(inputs: $inputs);
-        $magento->update(inputs: $inputs);
+        $magento->CX($inputs);
+        $magento->update($inputs);
         
-        return response()->json(data: $instance);
+        return response()->json($instance);
     }
     /**
      * Remove the specified resource from storage.
