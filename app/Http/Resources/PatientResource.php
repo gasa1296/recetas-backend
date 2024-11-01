ffd2892abbb83ab21ffb3e9f713c501181f1b09f
gasa1296 <gabriel.asa.1296@outlook.es>
2024-10-31 23:18:21 -0400

    fix

--- app/Http/Resources/PatientResource.php
+++ app/Http/Resources/PatientResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use App\Models\Prescription;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class PatientResource extends JsonResource
@@ -25,7 +26,7 @@ class PatientResource extends JsonResource
             'phone2' => $this->phone2,
             'birth_date' => $this->birth_date,
             'gender' => $this->gender,
-            'prescriptions' => PrescriptionResource::collection($this->prescriptions->where('user_id', '=', auth()->id()))
+            'prescriptions' => PrescriptionResource::collection($this->prescriptions)
         ];
     }
 }
