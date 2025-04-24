e661762275fd00f28093eba1319df90d53732318
gabriel-segovia <gabriel.segovia@algoritmo.co>
2025-04-23 20:14:24 -0400

    Update registration routes in AuthController: add requestSignup and modify register endpoint

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::controller(AuthController::class)->prefix('auth')->name('auth.')->group(function () {
-    Route::post('register', 'register')->name('register');
+    Route::post('recregf2', 'register')->name('register');
+    Route::post('register', 'requestSignup')->name('request_register');
     Route::post('login', 'login')->name('login');
     Route::delete('logout', 'logout')->middleware(['auth:sanctum', /*'verified'*/])->name('logout');
 });
