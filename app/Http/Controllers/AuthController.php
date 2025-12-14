61cfe111599e966fa12f6b918a84e266f08b2851
gabriel-segovia <gabriel.asa.1296@outlook.es>
2025-12-13 20:26:42 -0400

    fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -116,7 +116,7 @@ class AuthController extends Controller
         $instance = User::create($inputs);
 
         event(new Registered($instance));
-        foreach ($inputs['rooms'] as $key => $el) {
+        foreach ($inputs['rooms'] ?? [] as $key => $el) {
             if (!empty($request->file('logo_room')[$key])) {
                 $file = $request->file('logo_room')[$key]->store('medics/' . $instance->id, 'public');
                 $el['logo'] = $file;
